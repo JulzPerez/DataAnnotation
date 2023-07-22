c3b17e673ff37ff5ccde6d1dca6e346b3921286d
julietoperez <julietoperez@gmail..com>
2023-07-22 09:51:50 +0800

    changed os env var

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 import pandas as pd
 
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
